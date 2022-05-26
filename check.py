dc77205a49d12e57bf003b9b44fd422d07e69992
William Surya Wijaya <williamsw.gb@gmail.com>
2022-05-26 16:53:59 +0700

    feat(watermark-removal): update check to handle conversion of mask img visual properly

--- check.py
+++ check.py
@@ -7,35 +7,27 @@ from pdb import set_trace
 
 root_dataset = 'watermark_data/vietnam/annotations/training'
 
-segm_path = os.path.join(root_dataset, "ADE_train_00000001.png")
-mask_path = os.path.join(root_dataset, "chotot_1_gs/label_L.png")
-new_mask_path = os.path.join(root_dataset, "chotot_1_gs/label_P.png")
-# annot_path = os.path.join(root_dataset, "chotot_1_gs/label_gs.png")
+img_path = [
+    'chotot_1.png',
+    'batdongsan_1.png'
+]
 
-segm_img = Image.open(segm_path)
-mask_img = Image.open(mask_path)
-new_mask_img = Image.open(new_mask_path)
-# annot_img = Image.open(annot_path)
-# annot_img_L = Image.open(annot_path).convert('L')
+for img in img_path:
+    print(f'Image {img}\n')
+    mask_path = os.path.join(root_dataset, img)
+    mask_img = Image.open(mask_path).convert('L')
+
+    mask_arr = np.array(mask_img)
+    print(f"(Mask) Min: {np.min(mask_arr)}, Max: {np.max(mask_arr)}, Unique: {np.unique(mask_arr)}\n")
+
+    new_mask_arr = np.where(mask_arr>=1, 151, mask_arr)
+    print(f"(New Mask) Min: {np.min(new_mask_arr)}, Max: {np.max(new_mask_arr)}, Unique: {np.unique(new_mask_arr)}\n")
+
+    tensor_mask = torch.from_numpy(new_mask_arr).long() - 1
+    print(f"(Annot) Min: {torch.min(tensor_mask)}, Max: {torch.max(tensor_mask)}, Unique: {torch.unique(tensor_mask)}")
+
+    new_img = Image.fromarray(new_mask_arr)
+    new_img.save(f'watermark_data/new_{img}')
 
 # threshold = 2
 # new_img = new_mask_img.convert('L').point(lambda p: p > threshold and 1)
-
-segm_arr = np.array(segm_img)
-mask_arr = np.array(mask_img)
-new_mask_arr = np.array(new_mask_img)
-# annot_arr = np.array(annot_img)
-
-# mask_img_gs = mask_img.convert('L')
-__import__('pdb').set_trace()
-
-# new_img_arr = np.array(new_img)
-
-print(f"(Segm) Min: {np.min(segm_arr)}, Max: {np.max(segm_arr)}, Unique: {np.unique(segm_arr)}")
-print(f"(Mask) Min: {np.min(mask_arr)}, Max: {np.max(mask_arr)}, Unique: {np.unique(mask_arr)}")
-# print(f"(New Mask) Min: {np.min(new_mask_arr)}, Max: {np.max(new_mask_arr)}, Unique: {np.unique(new_mask_arr)}")
-print(f"(Annot) Min: {np.min(annot_arr)}, Max: {np.max(annot_arr)}, Unique: {np.unique(annot_arr)}")
-
-
-# new_annot_img = Image.fromarray(new_img_arr)
-# new_annot_img.save('watermark_data/vietnam/annotations/training/new_mask_1.png')

d4180a6d3a28e45da9f8c2eb11d59ed769859eba
William Surya Wijaya <williamsw.gb@gmail.com>
2022-06-07 02:57:40 +0800

    feat(watermark-removal): fix iou function use wrong pred result

--- mit_semseg/models/models.py
+++ mit_semseg/models/models.py
@@ -58,7 +58,8 @@ class SegmentationModule(SegmentationModuleBase):
 
             acc = self.pixel_acc(pred, seg_label)
             # Calculate iou when num class is available
-            iou = self.iou(pred, seg_label, self.num_class) if self.num_class else None
+            _, pred_iou = torch.max(pred, dim=1)
+            iou = self.iou(pred_iou, seg_label, self.num_class) if self.num_class else None
 
             return loss, acc, iou
         # inference
